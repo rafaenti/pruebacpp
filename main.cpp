147cd3aa2067ad1a61222ec645d70cd41062a081
Rafa Laguna <rafa.laguna@enti.cat>
2023-11-24 10:48:27 +0100

    Primer archivo en CPP

---
+++ main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+
+int main (int argc, char** argv){
+
+	//std::cout << argc << std::endl;
+
+	if (argc > 1){
+		std::cout << "Holi " << argv[1] << "!" << std::endl;
+	}
+	else{
+		std::cout << "Introduce un nombre como parámetro!!!" << std::endl;
+	}
+
+	return 0;
+}
